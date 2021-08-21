d11763412afab73303a8351f2d08d1aaa28a63c8
Gumbraise <vikiclashonyt@gmail.com>
2021-08-21 22:23:53 +0200

    added new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                huawei: {
+                    darker: '#0d1025',
+                    dark: '#1e223a',
+                    card: '#212940',
+                },
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
