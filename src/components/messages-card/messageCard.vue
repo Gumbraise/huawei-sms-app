f0cf0cb550a3012564dc4d83e36529a959dc8bd0
Gumbraise <vikiclashonyt@gmail.com>
2021-08-21 23:55:56 +0200

    That's great yes

--- src/components/messages-card/messageCard.vue
+++ src/components/messages-card/messageCard.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="bg-huawei-card p-6 rounded-lg shadow-lg text-white">
-    <h2 class="text-xl font-bold mb-2">{{ message.phone }}</h2>
-    <p class="truncate text-md">{{ message.content }}</p>
+  <div class="bg-huawei-card p-6 rounded-lg shadow-lg text-white relative">
+    <div class="mb-2 flex">
+      <h2 class="text-xl font-bold w-1/2">{{ message.phone }}</h2>
+      <p
+          class="text-md w-1/2 text-right text-gray-400"
+      >{{ message.date }}</p>
+    </div>
+
+    <p
+        :class="message.smstat ? 'truncate' : 'font-bold'"
+        class="text-md"
+    >{{ message.content }}</p>
   </div>
 </template>
 
