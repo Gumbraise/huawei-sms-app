f0a60b82a8784e3cf54f232503dfea764768f0aa
Gumbraise <vikiclashonyt@gmail.com>
2021-08-21 22:23:40 +0200

    added message card

---
+++ src/components/messages-card/messageCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-huawei-card p-6 rounded-lg shadow-lg text-white">
+    <h2 class="text-xl font-bold mb-2">+33771801718</h2>
+    <p class="truncate text-md">Comment il va le frérot ? Parce que là ça va commencer à dater un peu le bordel. On se fait quand une osirée ? J'ai trop hâte, aller la bise fiuls de pute.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "messageCard"
+}
+</script>
+
+<style scoped>
+
+</style>
